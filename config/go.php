7a0ece9c6f9ff4716cc5bdc4ed8d086c1a695017
Alexander Marunenko <sashamarunenko@gmail.com>
2026-04-29 11:28:44 +0300

    Настраивает докер под работу с delve для отладки GO-сервисов, меняет порты в конфиге go-роутов

--- config/go.php
+++ config/go.php
@@ -3,14 +3,14 @@
 return [
     'prices' => [
         'host' => env('GO_GAS_PRICES_URL', 'http://localhost'),
-        'port' => env('GO_GAS_PRICES_PORT', '8080'),
+        'port' => env('GO_GAS_PRICES_PORT', '8081'),
         'endpoints' => [
             'get_gas_prices' => '/api/get-gas-prices',
         ],
     ],
     'promotions' => [
         'host' => env('GO_PROMOTIONS_URL', 'http://localhost'),
-        'port' => env('GO_PROMOTIONS_PORT', '8080'),
+        'port' => env('GO_PROMOTIONS_PORT', '8082'),
         'endpoints' => [
             'check_promotions' => '/api/check-promotions',
         ],
